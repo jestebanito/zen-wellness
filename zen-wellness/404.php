91a30c496ca5c623a1e531c02de16112938ef5ab
jestebanito <137816026+jestebanito@users.noreply.github.com>
2024-07-05 16:33:13 -0700

    modified error 404 page

--- zen-wellness/404.php
+++ zen-wellness/404.php
@@ -12,46 +12,12 @@ get_header();
 
 	<main id="primary" class="site-main">
 
-		<section class="error-404 not-found">
+		<section class="error-404-not-found">
 			<header class="page-header">
 				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'zen-wellness' ); ?></h1>
+				<p class="desktop-message"><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links above?', 'zen-wellness' ); ?></p>
+				<p class="mobile-message"><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links in the  navigation bar below?', 'zen-wellness' ); ?></p>
 			</header><!-- .page-header -->
-
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'zen-wellness' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'zen-wellness' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$zen_wellness_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'zen-wellness' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$zen_wellness_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
 		</section><!-- .error-404 -->
 
 	</main><!-- #main -->
